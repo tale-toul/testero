82dbcb432b52ffd21ce59dac94ba1dc28d77a38c
Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>
2021-03-30 22:33:47 +0200

    Added function adrefiles with the task to add or remove files to reach the desired file size
    The adrefiles is not completed yet, so far:
    
    * Compiles a list of files for each directory d-<size>.  Only regular files starting with f-<number> are selected
    
    * Sorts the list numerically: f-1, f-2,....f-10.  For the sorting to work a function has been defined to compute the less operation for the sorting, this function compares the number suffixes of the files
    
    * Computes the total real size consumed by the files in the previous list
    
    * Computes the requested storage size for the current directory
    
    * Compares the requested and existing storage sizes and decides if add, removing of files is required, or nothing needs to be done
    
    * Gets the number assigned to the las file created, if any
    
    Creating the actual files is lacking functionality.
    
    Signed-off-by: Jose Ignacio Jerez Rodriguez <jjerezro@redhat.com>
    @@ -162,17 +168,97 @@ func CreateFiles(fS *FileCollection, ts int64, filelock chan int64) {
    +
    +//Returns a list of regular files with the correct name, in the directory specified, without
    +//directories or other types of files
    +func getFilesInDir(directory string) ([]os.FileInfo,error) {
    +	entries, err := ioutil.ReadDir(directory)
    +	if err != nil {
    +		log.Printf("getFilesInDir(): Error reading directory: %s",directory)
    +		return nil,err
    +	}
    +	var files []os.FileInfo
    +	for _,entry := range entries {
    +		if entry.Mode().IsRegular()  {
    +			match,_ := regexp.Match("f-[0-9]+",[]byte(entry.Name()))
    +			if match {
    +				files = append(files,entry)
    +			}
    +		}
    +	}
    +	return files,nil
    +}
    \ No newline at end of file

--- partdisk/partdisk.go
+++ partdisk/partdisk.go
@@ -3,9 +3,14 @@ package partdisk
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -22,7 +27,7 @@ type FileCollection struct {
 	fileAmmount []uint64
 	//Last request ID
 	flid int64
-	//Random id string
+	//Base dir made of random id string
 	frandi string
 }
 
@@ -89,7 +94,7 @@ func createTree(fc *FileCollection) error {
 //hlimit is the maximum size that can be requested
 func DefineFiles(tsize uint64, hilimit uint64, flS *FileCollection) error {
 	var nfiles, remain uint64
-	if tsize > hilimit || tsize == 0 {
+	if tsize > hilimit || tsize == 0 { //*THIS NEEDS TO BE CHANGED, BECAUSE THE REQUESTED SIZE MAY BE LOWER THAN THE ACTUAL USED SIZE IMPLYING A FILE DELETION, BUT GREATER THAN THE AVAILABLE SIZE WHICH WOULD BE REJECTED
 		return fmt.Errorf("Invalid total size %d.  High limit is %d bytes.", tsize, hilimit)
 	}
 	for index, fsize := range flS.fileSizes {
@@ -140,6 +145,7 @@ func GetDefFiles (fS *FileCollection) string {
 //Create or remove files to reach the requested number of files of each size
 func CreateFiles(fS *FileCollection, ts int64, filelock chan int64) {
 	var lt time.Time
+	var err error
 
 	select {
 	case <- time.After(5 * time.Second):
@@ -162,17 +168,97 @@ func CreateFiles(fS *FileCollection, ts int64, filelock chan int64) {
 		}
 	}
 	//Lock obtained proper, create/delete the files
-	err := createTree(fS)
+	err = createTree(fS)
 	if err != nil {
 		log.Printf("CreateFiles(): Error creating directory tree: %s\n%s\n",fS.frandi,err.Error())
 		return
 	}
 	//Directory tree created, now for the files
+	err = adrefiles(fS)
+	if err != nil {
+		log.Printf("CreateFiles(): Error creating file: %s\n",err.Error())
+		return
+	}
 
 	log.Printf("CreateFiles(): Request %d completed in %d seconds\n",ts,int64(time.Since(lt).Seconds()))
 }
-/*
-func adrefiles(fS *FileCollection) err {
 
+//Add or remove files match the files definition in the FileCollection struct
+func adrefiles(fS *FileCollection) error {
+	for index,value := range fS.fileSizes {
+		directory := fmt.Sprintf("%s/d-%d",fS.frandi,value)
+		fileList,err := getFilesInDir(directory)
+		if err != nil {
+			log.Printf("adrefiles(): Error listing directory: %s",directory)
+			return err
+		} 
+		//Sort the list of files
+		sort.Slice(fileList, func(i,j int) bool {
+			s1 := strings.TrimLeft(fileList[i].Name(),"f-")
+			s2 := strings.TrimLeft(fileList[j].Name(),"f-")
+			n1,_ := strconv.ParseInt(s1,10,32)
+			n2,_ := strconv.ParseInt(s2,10,32)
+			return n1 < n2
+		})
+		//Get the total size in bytes consumed by the files
+		var tfsize,rqsize,deltasize,fdelta uint64
+		for _,v := range fileList {
+			tfsize += uint64(v.Size())
+			log.Printf("File: %s - Size: %d",v.Name(),v.Size())
+		}
+		log.Printf("Total file size in dir %s: %d",directory,tfsize)
+		rqsize = fS.fileAmmount[index]*value
+		log.Printf("Requested size: %d",rqsize)
+		if tfsize > rqsize { //Need to remove files
+			deltasize = tfsize - rqsize
+			fdelta = deltasize / value
+			log.Printf("Need to remove %d bytes, %d files of size %d",deltasize,fdelta,value)
+		} else if tfsize < rqsize { //Need to create files
+			deltasize = rqsize - tfsize
+			fdelta = deltasize / value
+			log.Printf("Need to add %d bytes, %d files of size %d",deltasize,fdelta,value)
+		} else { //No need to add or remove anything 
+			log.Printf("No need to add or remove any files")
+		}
+
+		//Get the number of the last file created, 0 if none has been created
+		var lastfnum uint64
+		if len(fileList) > 0 {
+			lastfnum,_ = strconv.ParseUint(strings.TrimLeft(fileList[len(fileList)-1].Name(),"f-"),10,32)
+		} else {
+			lastfnum = 0 
+		}
+		log.Printf("Last file number: %d",lastfnum)
+
+		for n:=1;n<=int(fdelta);n++ {
+			filename := fmt.Sprintf("%s/d-%d/f-%d",fS.frandi,value,n)
+			f,err := os.Create(filename)
+			f.Close()
+			if err != nil {
+				log.Printf("adrefiles(): Error creating file: %s",filename)
+				return err
+			}
+		}
+	}
+	return nil
 }
-*/
+
+//Returns a list of regular files with the correct name, in the directory specified, without 
+//directories or other types of files
+func getFilesInDir(directory string) ([]os.FileInfo,error) {
+	entries, err := ioutil.ReadDir(directory)
+	if err != nil {
+		log.Printf("getFilesInDir(): Error reading directory: %s",directory)
+		return nil,err
+	}
+	var files []os.FileInfo
+	for _,entry := range entries {
+		if entry.Mode().IsRegular()  {
+			match,_ := regexp.Match("f-[0-9]+",[]byte(entry.Name()))
+			if match {
+				files = append(files,entry)
+			}
+		}
+	}
+	return files,nil
+}
